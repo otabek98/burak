1da8254afefbe6cf274b45a81a4d354f65a6eba3
Otabek <atabayev070@gmail.com>
2025-09-19 23:29:29 +0900

    feat: develop member schema

---
+++ src/libs/enums/member.enum.ts
@@ -0,0 +1,11 @@
+export enum MemberType{
+    USER = 'USER',
+    RESTAURANT = 'RESTSURANT'
+
+}
+export enum MemberStatus{
+    ACTIVE = 'ACTIVE',
+    BLOCK = 'BLOCK',
+    DELETE = 'DELETE',
+     
+}
