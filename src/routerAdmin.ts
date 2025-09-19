cdb7bb290abe1dab1c112bc6c82847d124cfb269
Otabek <atabayev070@gmail.com>
2025-09-19 23:07:58 +0900

    feat: member service model intigration

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -2,8 +2,8 @@ import  express from 'express'
 import restaurantController from './controllers/restaurant.controller'
 const routerAdmin = express.Router()
 
-routerAdmin.get('/', restaurantController.goHome )
-routerAdmin.get('/login', restaurantController.getLogin) 
-routerAdmin.get('/signup', restaurantController.getSignup)
+routerAdmin .get('/', restaurantController.goHome )
+routerAdmin .get('/login', restaurantController.getLogin) 
+routerAdmin .get('/signup', restaurantController.getSignup)
 
 export default routerAdmin;
